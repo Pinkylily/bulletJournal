8d5847b2e79a54c4270ad5d6415f993d19a74321
Louve <e.lescarret@bee-buzziness.com>
2024-02-07 13:38:59 +0100

    Feat(front): taskItem

---
+++ front/src/common/components/TaskItem/TaskItem.stories.tsx
@@ -0,0 +1,12 @@
+import TaskItem from "./TaskItem";
+
+export default {
+  component: TaskItem,
+  title: "TaskItem",
+};
+
+export const Default = {
+  args: {
+    title: "Tâche non planifié",
+  },
+};
