8a793d8b21f4777122bcd49a8c57ecf6cfe6d347
Louve <e.lescarret@bee-buzziness.com>
2024-02-07 13:42:03 +0100

    refacto(front): run prettier

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from "react-dom/client";
 
 const App = () => {
-    return <div>HELLO WORLD !</div>
-}
+  return <div>HELLO WORLD !</div>;
+};
 
 const container = document.getElementById("root");
 const root = createRoot(container);
