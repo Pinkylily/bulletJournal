dd00905ee2c98d24312ed6f9d05a6f0edd33290a
Louve <e.lescarret@bee-buzziness.com>
2024-02-07 12:40:19 +0100

    Feat(front): Add Accordion content and story

--- front/src/common/components/Accordion/Accordion.tsx
+++ front/src/common/components/Accordion/Accordion.tsx
@@ -1,10 +1,27 @@
-import React from "react"
+import React from "react";
+import MuiAccordion from "@mui/material/Accordion";
+import MuiAccordionSummary from "@mui/material/AccordionSummary";
+import MuiAccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 interface IAccordionProps {
+  title: string;
+  children: React.ReactNode;
 }
 
-const Accordion: React.FunctionComponent<IAccordionProps> = ({}) => {
-    return <></>
-}
+const Accordion: React.FunctionComponent<IAccordionProps> = ({ children }) => {
+  return (
+    <MuiAccordion defaultExpanded>
+      <MuiAccordionSummary
+        id="panel-header"
+        aria-controls="panel-content"
+        expandIcon={<ExpandMoreIcon />}
+      >
+        Header
+      </MuiAccordionSummary>
+      <MuiAccordionDetails>{children}</MuiAccordionDetails>
+    </MuiAccordion>
+  );
+};
 
-export default Accordion
+export default Accordion;
