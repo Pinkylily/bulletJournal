fbec61bc01ce9ddb145f8728bafb022c0b8e6ad5
Elisa Lescarret <e.s.lescarret@gmail.com>
2024-03-04 13:00:44 +0100

    feat(front): event item

--- front/src/common/components/TaskItem/TaskItem.tsx
+++ front/src/common/components/TaskItem/TaskItem.tsx
@@ -1,25 +1,15 @@
-import React from "react";
 import { ListItem, ListItemText } from "@mui/material";
-import { styled } from "@mui/material/styles";
+import React from "react";
+import BadgeItem from "../BadgeItem/BadgeItem";
 
 interface ITaskItemProps {
   title: string;
 }
 
-const TaskBadge = styled("div", {
-  name: "MUITaskBadge",
-})(({ theme }) => ({
-  backgroundColor: theme.palette.divider,
-  borderRadius: "2px",
-  height: "26px",
-  width: "4px",
-  marginRight: "10px",
-}));
-
 const TaskItem: React.FunctionComponent<ITaskItemProps> = ({ title }) => {
   return (
     <ListItem>
-      <TaskBadge />
+      <BadgeItem />
       <ListItemText primary={title} primaryTypographyProps={{ noWrap: true }} />
     </ListItem>
   );
